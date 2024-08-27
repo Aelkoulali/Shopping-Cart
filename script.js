e60124375e98aa8d82a3f596c33334f5eed3a10c
Aelkoulali <abdellah.elkoulali@gmx.fr>
2024-08-27 16:15:24 +0200

    Add button to the div

--- script.js
+++ script.js
@@ -97,6 +97,7 @@ const products = [
         <h2>${name}</h2>
         <p class="dessert-price">$${price}</p>
         <p class="product-category">${"Category: " }${category}</p>
+        <button id="${id}" class="btn add-to-cart-btn">${"Add to cart"}</button>
         </div>
         `;
     }

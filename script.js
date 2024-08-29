fe48aa103fd11b70f2188cd0ed492a4af8532334
Aelkoulali <abdellah.elkoulali@gmx.fr>
2024-08-29 10:26:59 +0200

    Update HTML

--- script.js
+++ script.js
@@ -144,6 +144,7 @@ calculateTotal(){
   const subTotal = this.items.reduce((total, item) => total + item.price,0); // initial value = 0
   const tax = this.calculateTaxes(subTotal);
   this.total = subTotal + tax;
+  cartSubTotal.textContent = `$${subTotal.toFixed(2)}`;
 }
 
 };

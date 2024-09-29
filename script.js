5e15cc390dda22e47ea4cff074a396b55186d971
Aelkoulali <abdellah.elkoulali@gmx.fr>
2024-09-29 18:56:30 +0200

    Declare variables

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-// Access to the id from the html document
+// Declare variables
 const cartContainer = document.getElementById("cart-container");
 const productsContainer = document.getElementById("products-container");
 const dessertCards = document.getElementById("dessert-card-container");
